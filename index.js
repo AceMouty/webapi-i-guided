caa3cb63c8a05da054d98843a7d7024d59fe1595
AceMouty <kyleguerrero85@gmail.com>
2019-09-30 12:09:30 -0500

    Added in some comments and notes
    created a notes md file for some simple notes
    added in comments to the indes.js file

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
+// import express
 const express = require('express');
+// create server
 const server = express()
+
+// create a route / req handler
+
+// root route
+server.get("/", (req, res) => {
+    
+})
+
+
+// server port
 const port = 8000;
+// run the server
 server.listen(port, () => console.log(` API RUNNING ON PORT ${port}`));
